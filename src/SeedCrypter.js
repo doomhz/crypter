2167b54801f94c9e8036562a41278e768ea13658
Dumitru Glavan <dumitru.gl@gmail.com>
2021-12-19 19:48:55 +0100

    Cleanup encryption algo

--- src/SeedCrypter.js
+++ src/SeedCrypter.js
@@ -3,8 +3,8 @@ const crypto = require('crypto');
 const { Buffer } = require('buffer/');
 
 const DERIVATION_ROUNDS = 200000;
-const HMAC_KEY_SIZE = 32;
-const PASSWORD_KEY_SIZE = 32;
+const PASSWORD_KEY_SIZE = 64;
+const SALT_LENGTH = 12;
 
 function pbkdf2(password, salt, rounds, bits) {
   return new Promise((resolve, reject) => {
@@ -17,109 +17,71 @@ function pbkdf2(password, salt, rounds, bits) {
   });
 }
 
-function deriveFromPassword(password, salt, rounds) {
+function deriveFromPassword(password, salt) {
   if (!password) {
     return Promise.reject(new Error('Failed deriving key: Password must be provided'));
   }
   if (!salt) {
     return Promise.reject(new Error('Failed deriving key: Salt must be provided'));
   }
-  if (!rounds || rounds <= 0 || typeof rounds !== 'number') {
-    return Promise.reject(new Error('Failed deriving key: Rounds must be greater than 0'));
-  }
-  const bits = (PASSWORD_KEY_SIZE + HMAC_KEY_SIZE) * 8;
-  return pbkdf2(password, salt, rounds, bits)
+  const bits = PASSWORD_KEY_SIZE * 8;
+  return pbkdf2(password, salt, DERIVATION_ROUNDS, bits)
     .then((derivedKeyData) => derivedKeyData.toString('hex'))
     .then((derivedKeyHex) => {
       const dkhLength = derivedKeyHex.length;
       const keyBuffer = Buffer.from(derivedKeyHex.substr(0, dkhLength / 2), 'hex');
-      const output = {
-        salt,
-        key: keyBuffer,
-        rounds,
-        hmac: Buffer.from(derivedKeyHex.substr(dkhLength / 2, dkhLength / 2), 'hex'),
-      };
-      return output;
+      return keyBuffer;
     });
-}
-
-function constantTimeCompare(val1, val2) {
-  let sentinel;
-  if (val1.length !== val2.length) {
-    return false;
-  }
-  for (let i = 0; i <= val1.length - 1; i += 1) {
-    sentinel |= val1.charCodeAt(i) ^ val2.charCodeAt(i);
-  }
-  return sentinel === 0;
 }
 
 function generateIV() {
   return Promise.resolve(Buffer.from(crypto.randomBytes(16)));
 }
 
-function generateSalt(length) {
-  if (length <= 0) {
-    return Promise.reject(
-      new Error(`Failed generating salt: Invalid length supplied: ${length}`),
-    );
-  }
+function generateSalt() {
   let output = '';
-  while (output.length < length) {
+  while (output.length < SALT_LENGTH) {
     output += crypto.randomBytes(3).toString('base64');
-    if (output.length > length) {
-      output = output.substr(0, length);
+    if (output.length > SALT_LENGTH) {
+      output = output.substr(0, SALT_LENGTH);
     }
   }
   return Promise.resolve(output);
 }
 
 function packageComponents(encryptedContent, components) {
-  return `$myencryption$${Object.keys(components).map((key) => `${key}=${components[key]}`).join(',')}$${encryptedContent}`;
+  return `${Object.keys(components).map((key) => `${key}=${components[key]}`).join(',')}$${encryptedContent}`;
 }
 
 function unpackageComponents(payload) {
-  const [, encryptor, componentsStr, encryptedContent] = payload.split('$');
-  if (encryptor !== 'myencryption') {
-    throw new Error('Failed decrypting: unrecognised encrypted payload');
-  }
+  const [componentsStr, encryptedContent] = payload.split('$');
   const components = componentsStr.split(',').reduce((output, item) => {
     const [key, value] = item.split('=');
     return Object.assign(output, {
       [key]: value,
     });
   }, {});
-  components.r = parseInt(components.r, 10);
   components.encryptedContent = encryptedContent;
   return components;
 }
 
 function encrypt(text, password) {
-  return generateSalt(12)
+  return generateSalt()
     .then((salt) => Promise.all([
       generateIV(),
       Promise.resolve(salt),
-      deriveFromPassword(password, salt, DERIVATION_ROUNDS),
+      deriveFromPassword(password, salt),
     ]))
     .then(([iv, salt, derivedKey]) => {
       const ivHex = iv.toString('hex');
-      const encryptTool = crypto.createCipheriv('aes-256-cbc', derivedKey.key, iv);
-      const hmacTool = crypto.createHmac('sha512', derivedKey.hmac);
+      const encryptTool = crypto.createCipheriv('aes-256-cbc', derivedKey, iv);
       // Perform encryption
       let encryptedContent = encryptTool.update(text, 'utf8', 'base64');
       encryptedContent += encryptTool.final('base64');
-      // Generate hmac
-      hmacTool.update(encryptedContent);
-      hmacTool.update(ivHex);
-      hmacTool.update(salt);
-      const hmacHex = hmacTool.digest('hex');
       // Output encrypted components
       const components = {
-        m: 'cbc',
-        h: hmacHex,
         i: ivHex,
         s: salt,
-        r: DERIVATION_ROUNDS,
       };
       return packageComponents(encryptedContent, components);
     });
@@ -132,24 +94,12 @@ function decrypt(encryptedString, password) {
     .then(() => {
       encryptedComponents = unpackageComponents(encryptedString);
       // console.log(JSON.stringify(encryptedComponents, undefined, 2));
-      return deriveFromPassword(password, encryptedComponents.s, encryptedComponents.r);
+      return deriveFromPassword(password, encryptedComponents.s);
     })
     .then((derivedKey) => {
       const iv = Buffer.from(encryptedComponents.i, 'hex');
-      const hmacData = encryptedComponents.h;
-      // Get HMAC tool
-      const hmacTool = crypto.createHmac('sha512', derivedKey.hmac);
-      // Generate the HMAC
-      hmacTool.update(encryptedComponents.encryptedContent);
-      hmacTool.update(encryptedComponents.i);
-      hmacTool.update(encryptedComponents.s);
-      const newHmaxHex = hmacTool.digest('hex');
-      // Check hmac for tampering
-      if (constantTimeCompare(hmacData, newHmaxHex) !== true) {
-        throw new Error('Authentication failed while decrypting content');
-      }
       // Decrypt
-      const decryptTool = crypto.createDecipheriv('aes-256-cbc', derivedKey.key, iv);
+      const decryptTool = crypto.createDecipheriv('aes-256-cbc', derivedKey, iv);
       const decryptedText = decryptTool.update(encryptedComponents.encryptedContent, 'base64', 'utf8');
       return `${decryptedText}${decryptTool.final('utf8')}`;
     });
